ab8ddb2bc090ed64aa25e474a24aa6de61036765
Julius Eerola <julius.eerola@tieto.com>
2020-07-12 16:09:59 +0300

    Add POST /orders unit tests

--- services/order-api/test/orders.test.ts
+++ services/order-api/test/orders.test.ts
@@ -2,14 +2,20 @@ import request, { Response } from 'supertest';
 import { initExpressApp } from '../src/app';
 import { Server } from 'http';
 import * as typeorm from 'typeorm';
+import axios from 'axios';
+import { logger } from '../src/utils';
 
 import ORDERS from './resources/orders.json';
 import ORDER_BY_ID from './resources/orderById.json';
+import CREATE_ORDER from './resources/createOrder.json';
+import PRODUCTS from './resources/products.json';
 
 describe('GET /orders', () => {
   let app: Server;
 
   beforeAll(() => {
+    logger.silent = true;
+
     // Mocks
     (typeorm as any).getRepository = jest.fn().mockReturnValue({
       find: async () => Promise.resolve(ORDERS),
@@ -19,6 +25,7 @@ describe('GET /orders', () => {
   });
 
   afterAll(() => {
+    logger.silent = false;
     app.close();
   });
 
@@ -48,6 +55,8 @@ describe('GET /orders/{orderId}', () => {
   let app: Server;
 
   beforeAll(() => {
+    logger.silent = true;
+
     // Mocks
     (typeorm as any).getRepository = jest.fn().mockReturnValue({
       findOne: async (id: number) => {
@@ -60,6 +69,7 @@ describe('GET /orders/{orderId}', () => {
   });
 
   afterAll(() => {
+    logger.silent = false;
     app.close();
   });
 
@@ -90,5 +100,66 @@ describe('GET /orders/{orderId}', () => {
       .set('x-user-id', 'testuser')
       .expect('Content-Type', /json/)
       .expect(404, done);
+  });
+});
+
+describe('POST /orders', () => {
+  let app: Server;
+
+  beforeAll(() => {
+    logger.silent = true;
+
+    // Mocks
+    (typeorm as any).getRepository = jest.fn().mockReturnValue({
+      save: jest.fn().mockResolvedValue({
+        ...CREATE_ORDER,
+        id: 1,
+      })
+    });
+
+    (axios as any).create = jest.fn().mockReturnValue({
+      get: jest
+        .fn()
+        .mockResolvedValueOnce({ data: PRODUCTS[0] })
+        .mockResolvedValueOnce({ data: PRODUCTS[1] }),
+    });
+
+    app = initExpressApp().listen(9000);
+  });
+
+  afterAll(() => {
+    logger.silent = false;
+    app.close();
+  });
+
+  it('should not accept orders from unauthenticated user', done => {
+    request(app)
+      .post('/orders')
+      .send(CREATE_ORDER)
+      .expect('Content-Type', /json/)
+      .expect(401, done);
+  });  
+
+  it('should not accept invalid order', done => {
+    request(app)
+      .post('/orders')
+      .set('x-user-id', 'testuser')
+      .send({ x: '2' })
+      .expect('Content-Type', /json/)
+      .expect(400, done);
+  });
+
+  it('should accept correct order', done => {
+    expect.assertions(1);
+
+    request(app)
+      .post('/orders')
+      .set('x-user-id', 'testuser')
+      .send(CREATE_ORDER)
+      .expect('Content-Type', /json/)
+      .expect((res: Response) => {
+        expect(res.body.id).toBe(1);
+      })
+      .expect(201, done);
   });
 });
